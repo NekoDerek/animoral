a599f8feecd49c826cb826894018442bcf2ab558
NekoDerek <49247588+NekoDerek@users.noreply.github.com>
2020-01-12 16:34:01 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   include SessionsHelper
   # Prevent CSRF attacks by raising an exception.
-  
   # For APIs, you may want to use :null_session instead.
 end
