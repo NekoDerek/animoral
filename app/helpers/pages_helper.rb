ada645ef1e46a4dc0dc7b0ce2d25eda52d4cb6f8
NekoDerek <senapiguru1488@gmail.com>
2020-04-25 17:41:05 +0300

    first try for DRY position

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,4 +4,12 @@
 module PagesHelper
   LINKS = [{ 'link_name': 'About', 'path': 'about' },
            { 'link_name': 'Contact', 'path': 'contact' }].freeze
+
+  def main
+    link_to 'Animoral', root_path
+  end
+
+  def derek
+    link_to 'Эдуард Навальный', "https://derek2.ru"
+  end
 end
