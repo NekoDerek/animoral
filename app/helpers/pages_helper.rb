1cf9a64c4bb6237fcb0f8fdfdfe64eb676c13fa5
NekoDerek <senapiguru1488@gmail.com>
2020-04-25 17:33:34 +0300

    refactor with DRY position

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# :nodoc:
 module PagesHelper
+  LINKS = [{ 'link_name': 'About', 'path': 'about' },
+           { 'link_name': 'Contact', 'path': 'contact' }].freeze
 end
