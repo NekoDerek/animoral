576d852cb7f1657585c0901b49c891603fe3444e
NekoDerek <senapiguru1488@gmail.com>
2020-04-25 17:10:02 +0300

    refactor sessions controller code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
+# :nodoc:
 class SessionsController < ApplicationController
 
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
